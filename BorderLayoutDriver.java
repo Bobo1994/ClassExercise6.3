9f9bb70cd355150e763ea0b461fed5679b05f49a
Bobo1994 <zhang60638@gmail.com>
2015-11-18 14:40:47 -0500

    Create BorderLayoutDriver.java

---
+++ BorderLayoutDriver.java
@@ -0,0 +1,10 @@
+
+public class BorderLayoutDriver {
+
+	public static void main(String[] args) {
+		SwingBorderLayouts test = new SwingBorderLayouts();
+		SwingGridLayouts test2 = new SwingGridLayouts();
+		SwingFlowLayouts test3 = new SwingFlowLayouts();
+	}
+
+}
